562b3bf8b223b9fd6ff7e76b9726aef8fe1fcf45
lcy0x1 <arthurlcy0x1@gmail.com>
2022-05-27 18:22:24 -0700

    add loot gen

--- src/main/java/dev/xkmc/playerdifficulty/init/data/DifficultyConfig.java
+++ src/main/java/dev/xkmc/playerdifficulty/init/data/DifficultyConfig.java
@@ -10,6 +10,7 @@ public class DifficultyConfig {
 	public static class Common {
 
 		public final ForgeConfigSpec.IntValue maxDifficulty;
+		public final ForgeConfigSpec.IntValue altDifficulty;
 
 		public final ForgeConfigSpec.DoubleValue enchantLevelFactor;
 		public final ForgeConfigSpec.DoubleValue armorChanceFactor;
@@ -31,56 +32,45 @@ public class DifficultyConfig {
 		Common(ForgeConfigSpec.Builder builder) {
 			builder.push("difficulty");
 			maxDifficulty = builder.comment("Maximum Difficulty")
-					.worldRestart()
 					.defineInRange("maxDifficulty", 100, 5, 10000);
+			altDifficulty = builder.comment("Change in difficulty when using up or down item")
+					.defineInRange("altDifficulty", 10, 1, 10000);
 			builder.pop();
 
 			builder.push("equipment");
 			enchantLevelFactor = builder.comment("Enchantment level factor")
-					.worldRestart()
 					.defineInRange("enchantLevelFactor", 0.6,0.001,  1);
 			armorChanceFactor = builder.comment("armor spawn chance factor")
-					.worldRestart()
 					.defineInRange("armorChanceFactor", 0.02,0.001,  1);
 			weaponChanceFactor = builder.comment("weapon spawn chance factor")
-					.worldRestart()
 					.defineInRange("weaponChanceFactor", 0.04,0.001,  1);
 			dropChance = builder.comment("equipment drop chance")
-					.worldRestart()
 					.defineInRange("dropChance", 0.085,0.085,  1);
 			builder.pop();
 
 			builder.push("champion");
 			championEnable = builder.comment("enable champion module in general")
-					.worldRestart()
 					.define("championEnable", true);
 			championsTierFactor = builder.comment("How many difficulties to match one tier")
-					.worldRestart()
 					.defineInRange("championsTierFactor", 10, 1, 100);
 			championsTierSeparation = builder.comment("To what power does tier spawn chance to differ")
-					.worldRestart()
 					.defineInRange("championsTierSeparation", 2, 1, 10);
 			builder.pop();
 
 			builder.push("apotheosis");
 			apotheosisEnable = builder.comment("enable apotheosis module in general (note: apotheosis deadly module is not completed yet)")
-					.worldRestart()
 					.define("apotheosisEnable", false);
 			apotheosisTierFactor = builder.comment("How many difficulties to match one tier")
-					.worldRestart()
 					.defineInRange("apotheosisTierFactor", 10, 1, 100);
 			apotheosisTierSeparation = builder.comment("To what power does tier spawn chance to differ")
-					.worldRestart()
 					.defineInRange("apotheosisTierSeparation", 2, 1, 10);
 			apotheosisTierChance = builder.comment("Successive chance to generate next tier item")
-					.worldRestart()
 					.defineInRange("apotheosisTierChance", 0.2, 0.01, 0.9);
 			builder.pop();
 
 			builder.push("scalingHealth");
-			scalingHealthFactor = builder.comment("ScalingHealth difficulty to PlayerDifficulty difficilty ratio")
-					.worldRestart()
-					.defineInRange("scalingHealthFactor", 1, 0.001, 1000);
+			scalingHealthFactor = builder.comment("ScalingHealth difficulty to PlayerDifficulty difficulty ratio")
+					.defineInRange("scalingHealthFactor", 0.4, 0.001, 1000);
 			builder.pop();
 
 		}
